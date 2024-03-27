ae1764be7d58fdc3b481b000a7c5450f09c7ada8
telontelvarost <telontelvarost@gmail.com>
2024-03-26 20:45:13 -0400

    UDOC-0 - Add a few more 404 challenge achievements

--- src/main/java/com/github/telvarost/whatareyouscoring/achievement/Ways404Achievements.java
+++ src/main/java/com/github/telvarost/whatareyouscoring/achievement/Ways404Achievements.java
@@ -13,7 +13,12 @@ public class Ways404Achievements {
     public static final List<Achievement> ACHIEVEMENTS = new ArrayList<>();
 
     public static final Achievement START_404 = make("start_404", ItemBase.book, 0, 0, null, false);
-    public static final Achievement ZOMBIES_KILLED = make("zombies_killed", ItemBase.feather, 0, 2, START_404, false);
+    public static final Achievement ZOMBIE_KILLED = make("zombie_killed", ItemBase.feather, -2, 2, START_404, false);
+    public static final Achievement SKELETON_KILLED = make("skeleton_killed", ItemBase.bone, -1, 2, START_404, false);
+    public static final Achievement SPIDER_KILLED = make("spider_killed", ItemBase.string, 0, 2, START_404, false);
+    public static final Achievement CREEPER_KILLED = make("creeper_killed", ItemBase.gunpowder, 1, 2, START_404, false);
+    public static final Achievement GHAST_KILLED = make("ghast_killed", ItemBase.snowball, 2, 2, START_404, false);
+    public static final Achievement ZOMBIE_PIGMAN_KILLED = make("zombie_pigman_killed", ItemBase.cookedPorkchop, 3, 2, START_404, false);
 
     private static Achievement make(String name, BlockBase icon, int x, int y, Achievement parent, boolean isChallenge) {
         Achievement achievement = new Achievement(ModHelper.ModHelperFields.ACHIEVEMENT_ID++, "whatareyouscoring:" + name, x, y, icon, parent);
