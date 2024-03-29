9687c7e0a1b9a1a150b8ddad3e34a8385b29e926
telontelvarost <telontelvarost@gmail.com>
2024-03-29 13:41:40 -0400

    UDOC-0 - Fix incompatibiliy with MiscTweaks

--- src/main/java/com/github/telvarost/whatareyouscoring/mixin/CreeperMixin.java
+++ src/main/java/com/github/telvarost/whatareyouscoring/mixin/CreeperMixin.java
@@ -2,12 +2,14 @@ package com.github.telvarost.whatareyouscoring.mixin;
 
 import com.github.telvarost.whatareyouscoring.Config;
 import com.github.telvarost.whatareyouscoring.ModHelper;
+import net.minecraft.entity.EntityBase;
 import net.minecraft.entity.monster.Creeper;
 import net.minecraft.entity.monster.MonsterBase;
 import net.minecraft.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Creeper.class)
 public class CreeperMixin extends MonsterBase {
@@ -17,20 +19,18 @@ public class CreeperMixin extends MonsterBase {
         this.texture = "/mob/creeper.png";
     }
 
-    @Redirect(
+    @Inject(
             method = "tryAttack",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/entity/monster/Creeper;isCharged()Z"
             )
     )
-    public boolean miscTweaks_tryAttackBeforeCreateExplosion(Creeper instance) {
+    public void miscTweaks_tryAttackBeforeCreateExplosion(EntityBase f, float par2, CallbackInfo ci) {
         if (Config.config.CHALLENGE_404_CONFIG.CHALLENGE_404_SCORING_ENABLED)
         {
             ModHelper.ModHelperFields.DETECT_CREEPER_EXPLOSION++;
         }
-
-        return instance.isCharged();
     }
 
 }
