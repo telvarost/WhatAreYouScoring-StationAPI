e8cac3fe43186224f74f50e531c8562eb69d4866
telontelvarost <telontelvarost@gmail.com>
2024-03-31 12:35:19 -0400

    UDOC-0 - Change display score on death to be toggleable in each config menu and remove old enum for it

--- src/main/java/com/github/telvarost/whatareyouscoring/Config.java
+++ src/main/java/com/github/telvarost/whatareyouscoring/Config.java
@@ -9,18 +9,6 @@ public class Config {
     public static ConfigFields config = new ConfigFields();
 
     public static class ConfigFields {
-        @ConfigName("Death Score To Display")
-        @Comment("Enable/Disable Scoring Modes In Their Config")
-        public ScoreDisplayEnum SCORING_DISPLAY_TYPE = ScoreDisplayEnum.BASIC_SCORE;
-
-        @ConfigName("Reset Score Instead With Difficulty Multipliers")
-        @Comment("Hard=0, Normal=0.5, Easy=0.75, Peaceful=1")
-        public Boolean SPECIAL_DEATH_EFFECT_ON_SCORE = false;
-
-        @ConfigName("Show Score On Begin Scoring Achievement")
-        @Comment("Score will display at the end of the tooltip")
-        public Boolean DISPLAY_SCORE_ON_BEGIN_ACHIEVEMENT = true;
-
         @ConfigCategory("Basic Score Config")
         public BasicScoreConfig BASIC_SCORE_CONFIG = new BasicScoreConfig();
 
@@ -29,9 +17,20 @@ public class Config {
 
         @ConfigCategory("404 Challenge Score Config")
         public Challenge404Config CHALLENGE_404_CONFIG = new Challenge404Config();
+
+        @ConfigName("Reset Score Instead With Difficulty Multipliers")
+        @Comment("Hard=0, Normal=0.5, Easy=0.75, Peaceful=1")
+        public Boolean SPECIAL_DEATH_EFFECT_ON_SCORE = false;
+
+        @ConfigName("Show Score On Begin Scoring Achievement")
+        @Comment("Score will display at the end of the tooltip")
+        public Boolean DISPLAY_SCORE_ON_BEGIN_ACHIEVEMENT = true;
     }
 
     public static class BasicScoreConfig {
+        @ConfigName("Display Basic Score On Death")
+        public Boolean DISPLAY_BASIC_SCORE_ON_DEATH = true;
+        
         @ConfigName("Enable Basic Scoring")
         @Comment("Reload world for changes to take effect")
         public Boolean BASIC_SCORING_ENABLED = true;
@@ -50,6 +49,9 @@ public class Config {
     }
 
     public static class DaysConfig {
+        @ConfigName("Display Days Score On Death")
+        public Boolean DISPLAY_DAYS_SCORE_ON_DEATH = false;
+
         @ConfigName("Enable Days Scoring")
         @Comment("Reload world for changes to take effect")
         public Boolean DAYS_SCORING_ENABLED = true;
@@ -65,6 +67,9 @@ public class Config {
     }
 
     public static class Challenge404Config {
+        @ConfigName("Display 404 Challenge Score On Death")
+        public Boolean DISPLAY_404_CHALLENGE_SCORE_ON_DEATH = false;
+
         @ConfigName("Enable 404 Challenge Scoring")
         @Comment("Reload world for changes to take effect")
         public Boolean CHALLENGE_404_SCORING_ENABLED = false;
--- src/main/java/com/github/telvarost/whatareyouscoring/mixin/DeathScreenMixin.java
+++ src/main/java/com/github/telvarost/whatareyouscoring/mixin/DeathScreenMixin.java
@@ -37,75 +37,48 @@ public class DeathScreenMixin extends ScreenBase {
             )
     )
     private void clientsideEssentials_renderDeathScreenText(Death instance, TextRenderer textRenderer, String s, int i, int j, int k) {
-        if (ScoreDisplayEnum.VANILLA == Config.config.SCORING_DISPLAY_TYPE) {
-            this.drawTextWithShadowCentred(textRenderer, s, i, j, k);
-        } else if (ScoreDisplayEnum.BASIC_SCORE == Config.config.SCORING_DISPLAY_TYPE) {
+        ArrayList scoresToDisplay = new ArrayList<Integer>();
+        ArrayList scoresToDisplayColor = new ArrayList<Character>();
+
+        if (Config.config.BASIC_SCORE_CONFIG.DISPLAY_BASIC_SCORE_ON_DEATH) {
             /** - Get basic score */
             int currentScore = ModHelper.ModHelperFields.DEATH_SCORE_BASIC_MODE;
             if (Config.config.SPECIAL_DEATH_EFFECT_ON_SCORE) {
                 currentScore += ModHelper.ModHelperFields.PrevCumulativeBasicScore;
             }
-            this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7e" + currentScore, i, j, k);
-        } else if (ScoreDisplayEnum.DAYS_SCORE == Config.config.SCORING_DISPLAY_TYPE) {
+            scoresToDisplay.add(currentScore);
+            scoresToDisplayColor.add('e');
+        }
+
+        if (Config.config.DAYS_SCORE_CONFIG.DISPLAY_DAYS_SCORE_ON_DEATH) {
             /** - Get days survived */
             int currentScore = ModHelper.ModHelperFields.DEATH_SCORE_DAYS_MODE;
             if (Config.config.SPECIAL_DEATH_EFFECT_ON_SCORE) {
                 currentScore += ModHelper.ModHelperFields.PrevCumulativeDaysScore;
             }
-            this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7b" + currentScore, i, j, k);
-        } else if (ScoreDisplayEnum.CHALLENGE_404 == Config.config.SCORING_DISPLAY_TYPE) {
+            scoresToDisplay.add(currentScore);
+            scoresToDisplayColor.add('b');
+        }
+
+        if (Config.config.CHALLENGE_404_CONFIG.DISPLAY_404_CHALLENGE_SCORE_ON_DEATH) {
             /** - Get 404 challenge score */
             int currentScore = ModHelper.ModHelperFields.DEATH_SCORE_404_MODE;
             if (Config.config.SPECIAL_DEATH_EFFECT_ON_SCORE) {
                 currentScore += ModHelper.ModHelperFields.PrevCumulative404Score;
             }
-            this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7c" + currentScore, i, j, k);
-        } else if (ScoreDisplayEnum.ALL_WAYS == Config.config.SCORING_DISPLAY_TYPE) {
-            ArrayList scoresToDisplay = new ArrayList<Integer>();
-            ArrayList scoresToDisplayColor = new ArrayList<Character>();
+            scoresToDisplay.add(currentScore);
+            scoresToDisplayColor.add('c');
+        }
 
-            if (Config.config.BASIC_SCORE_CONFIG.BASIC_SCORING_ENABLED) {
-                /** - Get basic score */
-                int currentScore = ModHelper.ModHelperFields.DEATH_SCORE_BASIC_MODE;
-                if (Config.config.SPECIAL_DEATH_EFFECT_ON_SCORE) {
-                    currentScore += ModHelper.ModHelperFields.PrevCumulativeBasicScore;
-                }
-                scoresToDisplay.add(currentScore);
-                scoresToDisplayColor.add('e');
-            }
-
-            if (Config.config.DAYS_SCORE_CONFIG.DAYS_SCORING_ENABLED) {
-                /** - Get days survived */
-                int currentScore = ModHelper.ModHelperFields.DEATH_SCORE_DAYS_MODE;
-                if (Config.config.SPECIAL_DEATH_EFFECT_ON_SCORE) {
-                    currentScore += ModHelper.ModHelperFields.PrevCumulativeDaysScore;
-                }
-                scoresToDisplay.add(currentScore);
-                scoresToDisplayColor.add('b');
-            }
-
-            if (Config.config.CHALLENGE_404_CONFIG.CHALLENGE_404_SCORING_ENABLED) {
-                /** - Get 404 challenge score */
-                int currentScore = ModHelper.ModHelperFields.DEATH_SCORE_404_MODE;
-                if (Config.config.SPECIAL_DEATH_EFFECT_ON_SCORE) {
-                    currentScore += ModHelper.ModHelperFields.PrevCumulative404Score;
-                }
-                scoresToDisplay.add(currentScore);
-                scoresToDisplayColor.add('c');
-            }
-
-            if (3 == scoresToDisplay.size()) {
-                this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7" + scoresToDisplayColor.get(0) + scoresToDisplay.get(0), i, j - 10, k);
-                this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7" + scoresToDisplayColor.get(1) + scoresToDisplay.get(1), i, j, k);
-                this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7" + scoresToDisplayColor.get(2) + scoresToDisplay.get(2), i, j + 10, k);
-            } else if (2 == scoresToDisplay.size()) {
-                this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7" + scoresToDisplayColor.get(0) + scoresToDisplay.get(0), i, j - 5, k);
-                this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7" + scoresToDisplayColor.get(1) + scoresToDisplay.get(1), i, j + 5, k);
-            } else if (1 == scoresToDisplay.size()) {
-                this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7" + scoresToDisplayColor.get(0) + scoresToDisplay.get(0), i, j, k);
-            } else {
-                this.drawTextWithShadowCentred(textRenderer, s, i, j, k);
-            }
+        if (3 == scoresToDisplay.size()) {
+            this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7" + scoresToDisplayColor.get(0) + scoresToDisplay.get(0), i, j - 10, k);
+            this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7" + scoresToDisplayColor.get(1) + scoresToDisplay.get(1), i, j, k);
+            this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7" + scoresToDisplayColor.get(2) + scoresToDisplay.get(2), i, j + 10, k);
+        } else if (2 == scoresToDisplay.size()) {
+            this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7" + scoresToDisplayColor.get(0) + scoresToDisplay.get(0), i, j - 5, k);
+            this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7" + scoresToDisplayColor.get(1) + scoresToDisplay.get(1), i, j + 5, k);
+        } else if (1 == scoresToDisplay.size()) {
+            this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7" + scoresToDisplayColor.get(0) + scoresToDisplay.get(0), i, j, k);
         } else {
             this.drawTextWithShadowCentred(textRenderer, s, i, j, k);
         }
