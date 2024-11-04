306384d1ce8c034d09e22dc503fc01e7a8e09a9d
telvarost <telontelvarost@gmail.com>
2024-11-03 21:45:04 -0500

    Migrate to GCAPI3

--- src/main/java/com/github/telvarost/whatareyouscoring/Config.java
+++ src/main/java/com/github/telvarost/whatareyouscoring/Config.java
@@ -1,87 +1,98 @@
 package com.github.telvarost.whatareyouscoring;
 
-import blue.endless.jankson.Comment;
-import net.glasslauncher.mods.api.gcapi.api.*;
+import net.glasslauncher.mods.gcapi3.api.*;
 
 public class Config {
 
-    @GConfig(value = "config", visibleName = "WhatAreYouScoring")
+    @ConfigRoot(value = "config", visibleName = "WhatAreYouScoring")
     public static ConfigFields config = new ConfigFields();
 
     public static class ConfigFields {
-        @ConfigCategory("Basic Score Config")
+        @ConfigCategory(name = "Basic Score Config")
         public BasicScoreConfig BASIC_SCORE_CONFIG = new BasicScoreConfig();
 
-        @ConfigCategory("Days Score Config")
+        @ConfigCategory(name = "Days Score Config")
         public DaysConfig DAYS_SCORE_CONFIG = new DaysConfig();
 
-        @ConfigCategory("404 Challenge Score Config")
+        @ConfigCategory(name = "404 Challenge Score Config")
         public Challenge404Config CHALLENGE_404_CONFIG = new Challenge404Config();
 
-        @ConfigName("Reset Score Instead With Difficulty Multipliers")
-        @Comment("Hard=0, Normal=0.5, Easy=0.75, Peaceful=1")
+        @ConfigEntry(
+                name = "Reset Score Instead With Difficulty Multipliers",
+                description = "Hard=0, Normal=0.5, Easy=0.75, Peaceful=1"
+        )
         public Boolean SPECIAL_DEATH_EFFECT_ON_SCORE = false;
 
-        @ConfigName("Show Score On Begin Scoring Achievement")
-        @Comment("Score will display at the end of the tooltip")
+        @ConfigEntry(
+                name = "Show Score On Begin Scoring Achievement",
+                description = "Score will display at the end of the tooltip"
+        )
         public Boolean DISPLAY_SCORE_ON_BEGIN_ACHIEVEMENT = true;
     }
 
     public static class BasicScoreConfig {
-        @ConfigName("Display Basic Score On Death")
+        @ConfigEntry(name = "Display Basic Score On Death")
         public Boolean DISPLAY_BASIC_SCORE_ON_DEATH = true;
 
-        @ConfigName("Enable Basic Scoring")
-        @Comment("Reload world for changes to take effect")
+        @ConfigEntry(
+                name = "Enable Basic Scoring",
+                description = "Reload world for changes to take effect"
+        )
         public Boolean BASIC_SCORING_ENABLED = true;
 
-        @ConfigName("Score: +1 From Each Block Placed")
+        @ConfigEntry(name = "Score: +1 From Each Block Placed")
         public Boolean ADD_SCORE_ON_BLOCK_PLACED = true;
 
-        @ConfigName("Score: +1 From Each Block Removed")
+        @ConfigEntry(name = "Score: +1 From Each Block Removed")
         public Boolean ADD_SCORE_ON_BLOCK_REMOVED = true;
 
-        @ConfigName("Score: +1 From Each Monster Mob Killed")
+        @ConfigEntry(name = "Score: +1 From Each Monster Mob Killed")
         public Boolean ADD_SCORE_ON_MONSTER_KILLED = true;
 
-        @ConfigName("Score: +1 From Each Passive Mob Killed")
+        @ConfigEntry(name = "Score: +1 From Each Passive Mob Killed")
         public Boolean ADD_SCORE_ON_PASSIVE_KILLED = true;
     }
 
     public static class DaysConfig {
-        @ConfigName("Display Days Score On Death")
+        @ConfigEntry(name = "Display Days Score On Death")
         public Boolean DISPLAY_DAYS_SCORE_ON_DEATH = false;
 
-        @ConfigName("Enable Days Scoring")
-        @Comment("Reload world for changes to take effect")
+        @ConfigEntry(
+                name = "Enable Days Scoring",
+                description = "Reload world for changes to take effect"
+        )
         public Boolean DAYS_SCORING_ENABLED = true;
 
-        @ConfigName("Score: +1 From Each Day Survived")
+        @ConfigEntry(name = "Score: +1 From Each Day Survived")
         public Boolean ADD_SCORE_FOR_EACH_DAY_SURVIVED = true;
 
-        @ConfigName("Score: +25 For Every 100 Days Survived")
+        @ConfigEntry(name = "Score: +25 For Every 100 Days Survived")
         public Boolean ADD_100_DAYS_BONUS_SCORE = false;
 
-        @ConfigName("Score: +100 For Every 365 Days Survived")
+        @ConfigEntry(name = "Score: +100 For Every 365 Days Survived")
         public Boolean ADD_YEAR_BONUS_SCORE = false;
     }
 
     public static class Challenge404Config {
-        @ConfigName("Display 404 Challenge Score On Death")
+        @ConfigEntry(name = "Display 404 Challenge Score On Death")
         public Boolean DISPLAY_404_CHALLENGE_SCORE_ON_DEATH = false;
 
-        @ConfigName("Enable 404 Challenge Scoring")
-        @Comment("Reload world for changes to take effect")
+        @ConfigEntry(
+                name = "Enable 404 Challenge Scoring",
+                description = "Reload world for changes to take effect"
+        )
         public Boolean CHALLENGE_404_SCORING_ENABLED = false;
 
-        @ConfigName("Enable Hard Mode Multiplier")
+        @ConfigEntry(name = "Enable Hard Mode Multiplier")
         public Boolean ENABLE_HARD_MODE_MULTIPLIER = true;
 
-        @ConfigName("Never Sleep/Wear Armor Gives Points")
-        @Comment("When false they subtract points when failed")
+        @ConfigEntry(
+                name = "Never Sleep/Wear Armor Gives Points",
+                description = "When false they subtract points when failed"
+        )
         public Boolean POSITIVE_NEVER_SLEEP_WEAR_ARMOR = true;
 
-        @ConfigName("Score Mob Kills")
+        @ConfigEntry(name = "Score Mob Kills")
         public Boolean SCORE_MOB_KILLS_404 = true;
     }
 }
--- src/main/java/com/github/telvarost/whatareyouscoring/ScoreDisplayEnumFactory.java
+++ src/main/java/com/github/telvarost/whatareyouscoring/ScoreDisplayEnumFactory.java
@@ -1,21 +1,19 @@
 package com.github.telvarost.whatareyouscoring;
 
-import blue.endless.jankson.JsonElement;
-import blue.endless.jankson.JsonPrimitive;
 import com.google.common.collect.ImmutableMap;
-import net.glasslauncher.mods.api.gcapi.api.ConfigFactoryProvider;
-import net.glasslauncher.mods.api.gcapi.api.MaxLength;
-import net.glasslauncher.mods.api.gcapi.impl.NonFunction;
-import net.glasslauncher.mods.api.gcapi.impl.config.ConfigEntry;
-import net.glasslauncher.mods.api.gcapi.impl.config.entry.EnumConfigEntry;
+import net.glasslauncher.mods.gcapi3.api.*;
+import net.glasslauncher.mods.gcapi3.impl.SeptFunction;
+import net.glasslauncher.mods.gcapi3.impl.object.ConfigEntryHandler;
+import net.glasslauncher.mods.gcapi3.impl.object.entry.EnumConfigEntryHandler;
 
 import java.lang.reflect.*;
 import java.util.function.*;
 
 public class ScoreDisplayEnumFactory implements ConfigFactoryProvider {
+
     @Override
-    public void provideLoadFactories(ImmutableMap.Builder<Type, NonFunction<String, String, String, Field, Object, Boolean, Object, Object, MaxLength, ConfigEntry<?>>> immutableBuilder) {
-        immutableBuilder.put(ScoreDisplayEnum.class, ((id, name, description, parentField, parentObject, isMultiplayerSynced, enumOrOrdinal, defaultEnum, maxLength) ->
+    public void provideLoadFactories(ImmutableMap.Builder<Type, SeptFunction<String, ConfigEntry, Field, Object, Boolean, Object, Object, ConfigEntryHandler<?>>> immutableBuilder) {
+        immutableBuilder.put(ScoreDisplayEnum.class, ((id, configEntry, parentField, parentObject, isMultiplayerSynced, enumOrOrdinal, defaultEnum) ->
         {
             int enumOrdinal;
             if(enumOrOrdinal instanceof Integer ordinal) {
@@ -24,18 +22,12 @@ public class ScoreDisplayEnumFactory implements ConfigFactoryProvider {
             else {
                 enumOrdinal = ((ScoreDisplayEnum) enumOrOrdinal).ordinal();
             }
-            return new EnumConfigEntry<ScoreDisplayEnum>(id, name, description, parentField, parentObject, isMultiplayerSynced, enumOrdinal, ((ScoreDisplayEnum) defaultEnum).ordinal(), ScoreDisplayEnum.class);
+            return new EnumConfigEntryHandler<ScoreDisplayEnum>(id, configEntry, parentField, parentObject, isMultiplayerSynced, enumOrdinal, ((ScoreDisplayEnum) defaultEnum).ordinal(), ScoreDisplayEnum.class);
         }));
     }
 
     @Override
-    public void provideSaveFactories(ImmutableMap.Builder<Type, Function<Object, JsonElement>> immutableBuilder) {
-        immutableBuilder.put(ScoreDisplayEnum.class, enumEntry -> new JsonPrimitive(((ScoreDisplayEnum) enumEntry).ordinal()));
-    }
-
-    @Override
-    public void provideLoadTypeAdapterFactories(@SuppressWarnings("rawtypes") ImmutableMap.Builder<Type, Class> immutableBuilder) {
-        immutableBuilder.put(ScoreDisplayEnum.class, Integer.class);
+    public void provideSaveFactories(ImmutableMap.Builder<Type, Function<Object, Object>> immutableBuilder) {
+        immutableBuilder.put(ScoreDisplayEnum.class, enumEntry -> enumEntry);
     }
 }
-
